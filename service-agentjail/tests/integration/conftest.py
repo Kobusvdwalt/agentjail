2a2fd6b238a7fd653a45003b64fd5f9b95813d2c
Kobus Van der Walt <kobusvdwalt9@gmail.com>
2026-05-11 19:02:02 +0200

    fix: run integration test container as root so nsjail can manipulate mounts

--- service-agentjail/tests/integration/conftest.py
+++ service-agentjail/tests/integration/conftest.py
@@ -31,7 +31,9 @@ def container(image):
         .with_exposed_ports(8000)
         .with_env_file(str(Path(SERVICE_DIR) / "default.env"))
         .with_kwargs(
-            privileged=True,
+            cap_add=["SYS_ADMIN"],
+            security_opt=["apparmor=unconfined", "seccomp=unconfined"],
+            user="root",
         )
         .waiting_for(
             LogMessageWaitStrategy("Application startup complete").with_startup_timeout(
